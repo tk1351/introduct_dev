f7691dfdeda7ef4c3b54b1c5fc1fcd16a065db2e
tk1351 <takakikitsui0111@gmail.com>
2021-03-18 20:59:48 +0900

    connected mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8080
+const connectDB = require('./config/db')
+
+// Connect DB
+connectDB()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
