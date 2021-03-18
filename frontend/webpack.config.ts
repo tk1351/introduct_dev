1a5bf45f2b41369ec3ebac0963884a0442218ae5
tk1351 <takakikitsui0111@gmail.com>
2021-03-18 19:26:30 +0900

    set up front and backend

---
+++ frontend/webpack.config.ts
@@ -0,0 +1,55 @@
+import path from 'path'
+import webpack, { Configuration } from 'webpack'
+
+const config: Configuration = {
+  context: path.join(__dirname, 'src'),
+  entry: './index.tsx',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/assets',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.jpg$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, 'static'),
+    open: true,
+    port: 3000,
+    proxy: {
+      '/api': 'http://backend:8080',
+    },
+  },
+}
+
+export default config
