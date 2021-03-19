093871523377873319785fbe2985a87b8c7cc9d1
tk1351 <takakikitsui0111@gmail.com>
2021-03-19 13:11:19 +0900

    added register useState

--- frontend/src/components/auth/Register.tsx
+++ frontend/src/components/auth/Register.tsx
@@ -1,7 +1,88 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
-  return <div>Register</div>
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  })
+
+  const { name, email, password, password2 } = formData
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (password !== password2) {
+      console.log('確認用パスワードが異なります')
+    } else {
+      console.log(formData)
+    }
+  }
+
+  return (
+    <Fragment>
+      <h1 className="large text-primary">ユーザー登録</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> アカウントを作成する
+      </p>
+      <form className="form" onSubmit={(e) => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="ユーザー名"
+            name="name"
+            value={name}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="メールアドレス"
+            name="email"
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
+          <small className="form-text">
+            Gravatarを使用している場合は、Gravatarのメールアドレスを入力してください
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="パスワード"
+            name="password"
+            minLength={6}
+            value={password}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="確認用パスワード"
+            name="password2"
+            minLength={6}
+            value={password2}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        既にアカウントを持っていますか？ <Link to="/login">ログイン</Link>
+      </p>
+    </Fragment>
+  )
 }
 
 export default Register
