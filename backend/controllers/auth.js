449bb1d43db6c30200863efb6cd35407cbf790db
tk1351 <takakikitsui0111@gmail.com>
2021-03-19 09:53:33 +0900

    added login func

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,4 +1,8 @@
 const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+const config = require('config')
+const jwt = require('jsonwebtoken')
+const { validationResult } = require('express-validator')
 
 module.exports = {
   getAuthUser: async (req, res) => {
@@ -8,6 +12,51 @@ module.exports = {
     } catch (err) {
       console.error(err)
       return res.status(500).send('Server Error')
+    }
+  },
+  loginUser: async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { email, password } = req.body
+
+    try {
+      // ユーザーが存在するか確認
+      let user = await User.findOne({ email })
+      if (!user) {
+        return res
+          .status(400)
+          .json({ erros: [{ msg: 'ユーザーが存在しません' }] })
+      }
+
+      // DBのパスワードと比較する
+      const isMatch = await bcrypt.compare(password, user.password)
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'パスワードが正しくありません' }] })
+      }
+
+      // jwtを返す
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      }
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err
+          return res.json({ token })
+        }
+      )
+    } catch (err) {
+      console.error(err.message)
+      res.status(500).send('Server error')
     }
   },
 }
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,16 @@ const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/auth')
 const auth = require('../middleware/auth')
+const { check } = require('express-validator')
 
 router.get('/', auth, authController.getAuthUser)
+router.post(
+  '/',
+  [
+    check('email', '正しいメールアドレスを入力してください').isEmail(),
+    check('password', 'パスワードを入力してください').exists(),
+  ],
+  authController.loginUser
+)
 
 module.exports = router
