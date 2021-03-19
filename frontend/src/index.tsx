ff66a385b77d74e654b13b81b40ca45fa6aa4429
tk1351 <takakikitsui0111@gmail.com>
2021-03-19 13:18:15 +0900

    create redux store

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import App from './App'
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 
 ReactDom.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('app')
 )
